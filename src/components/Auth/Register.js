2583048634a2f340f31a035b3c894706281489a3
Varun S Athreya <varun.sathreya@gmail.com>
2020-08-03 03:42:10 +0530

    Created form validation for  onSubmit

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -18,30 +18,81 @@ class Register extends Component {
         email: "",
         password: "",
         passwordConfirmation: "",
+        errors: [],
     };
+
+    isFormValid = () => {
+        let errors = [];
+        let error;
+
+        if (this.isFormEmpty(this.state)) {
+            //error
+            error = { message: "Fill in all Fields" };
+            this.setState({ errors: [...errors, error] });
+            return false;
+        } else if (!this.isPasswordValid(this.state)) {
+            //error
+            error = { message: "Password is invalid" };
+            this.setState({ errors: [...errors, error] });
+            return false;
+        } else {
+            // form valid
+            return true;
+        }
+    };
+
+    isFormEmpty = ({ username, email, password, passwordConfirmation }) => {
+        return (
+            !username.length ||
+            !email.length ||
+            !password.length ||
+            !passwordConfirmation.length
+        );
+    };
+
+    isPasswordValid = ({ password, passwordConfirmation }) => {
+        if (password.length < 0 || passwordConfirmation.length < 0) {
+            return false;
+        } else if (password !== passwordConfirmation) {
+            return false;
+        } else {
+            return true;
+        }
+    };
+
+    displayErrors = (errors) =>
+        errors.map((error, i) => <p key={i}>{error.message}</p>);
 
     handleChange = (event) => {
         this.setState({ [event.target.name]: event.target.value });
     };
 
     handleSubmit = (event) => {
-        event.preventDefault();
-        firebase
-            .auth()
-            .createUserWithEmailAndPassword(
-                this.state.email,
-                this.state.password
-            )
-            .then((createdUser) => {
-                console.log(createdUser);
-            })
-            .catch((err) => {
-                console.error(err);
-            });
+        if (this.isFormValid()) {
+            event.preventDefault();
+            firebase
+                .auth()
+                .createUserWithEmailAndPassword(
+                    this.state.email,
+                    this.state.password
+                )
+                .then((createdUser) => {
+                    console.log(createdUser);
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
+        }
     };
 
     render() {
-        const { username, email, password, passwordConfirmation } = this.state;
+        const {
+            username,
+            email,
+            password,
+            passwordConfirmation,
+            errors,
+        } = this.state;
 
         return (
             <Grid textAlign="center" verticalAlign="middle" className="app">
@@ -101,8 +152,14 @@ class Register extends Component {
                             </Button>
                         </Segment>
                     </Form>
+                    {errors.length > 0 && (
+                        <Message error>
+                            <h3>Error</h3>
+                            {this.displayErrors(errors)}
+                        </Message>
+                    )}
                     <Message>
-                        Already a user? <Link to="/login">Login</Link>
+                        Already a user? <Link to="/login"> Login</Link>
                     </Message>
                 </Grid.Column>
             </Grid>
