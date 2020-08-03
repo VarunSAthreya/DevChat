a31fa7ad05979520b0c2f715b494a205950a767e
Varun S Athreya <varun.sathreya@gmail.com>
2020-08-03 21:36:37 +0530

    Introuced Redux and Saved Logged in user to redux

--- src/index.js
+++ src/index.js
@@ -12,13 +12,24 @@ import * as serviceWorker from "./serviceWorker";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import firebase from "./firebase";
+import rootReducer from "./reducers";
+import { setUser } from "./actions";
 
 import "semantic-ui-css/semantic.min.css";
+
+// REDUX
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 class Root extends React.Component {
     componentDidMount() {
         firebase.auth().onAuthStateChanged((user) => {
             if (user) {
+                this.props.setUser(user);
+
                 this.props.history.push("/");
             }
         });
@@ -35,12 +46,14 @@ class Root extends React.Component {
     }
 }
 
-const RootWithAuth = withRouter(Root);
+const RootWithAuth = withRouter(connect(null, { setUser })(Root));
 
 ReactDOM.render(
-    <Router>
-        <RootWithAuth />
-    </Router>,
+    <Provider store={store}>
+        <Router>
+            <RootWithAuth />
+        </Router>
+    </Provider>,
     document.getElementById("root")
 );
 
