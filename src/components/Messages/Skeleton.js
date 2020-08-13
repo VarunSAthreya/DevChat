35dfe8c6c3444e7eaf656dfb58973c11dfe0e1aa
Varun S Athreya <varun.sathreya@gmail.com>
2020-08-14 02:53:24 +0530

    fixed seleton avatar class name error

--- src/components/Messages/Skeleton.js
+++ src/components/Messages/Skeleton.js
@@ -3,7 +3,7 @@ import React from "react";
 const Skeleton = () => {
     return (
         <div className="skeleton">
-            <div className="skeleton__avattar"></div>
+            <div className="skeleton__avatar"></div>
             <div className="skeleton__author"></div>
             <div className="skeleton__details"></div>
         </div>
